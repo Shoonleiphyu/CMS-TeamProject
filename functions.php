eb5f62d2f03c133ee87cb91b93df003ae38a3c4c
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2024-01-10 13:20:26 +0800

    color changes

--- functions.php
+++ functions.php
@@ -1,6 +1,21 @@
 <?php
-function custom_theme_setup() {
-    register_nav_menu( 'sidebar-menu', __( 'Sidebar Menu', 'my-theme' ) );
+
+function maitheme_reegister_styles(){
+    $version =wp_get_theme()->get('Version');
+    wp_enqueue_style('maitheme-style', get_template_directory_uri()."/style.css", array('maitheme-bootstrap'), $version,'all');
+    wp_enqueue_style('maitheme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '1.0','all');
+    wp_enqueue_style('maitheme-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '1.0','all');
 }
-add_action( 'after_setup_theme', 'my_theme_setup' );
+
+add_action('wp_enqueue_scripts', 'maitheme_reegister_styles');
+
+function maitheme_reegister_scripts(){
+    wp_enqueue_style('maitheme-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_style('maitheme-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_style('maitheme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '3.4.1', true);
+    wp_enqueue_style('maitheme-main', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'maitheme_reegister_scripts');
+
 ?>
