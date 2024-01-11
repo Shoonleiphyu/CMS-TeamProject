50db35e18b7c08167a87e7c2ceafe2b3100a3049
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2024-01-12 01:04:28 +0800

    post_section

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function maitheme_theme_support(){
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'maitheme_theme_support');
 
